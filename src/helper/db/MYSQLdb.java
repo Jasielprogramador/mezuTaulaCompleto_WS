2c6d66f69980b4c72aca770848b5beec6a8cfe36
Asier <asierrosa@gmail.com>
2021-03-18 10:02:56 +0100

    Update MYSQLdb.java

--- src/helper/db/MYSQLdb.java
+++ src/helper/db/MYSQLdb.java
@@ -17,80 +17,73 @@ public class MYSQLdb {
 	
 	private Connection conn;
 	
-	public MYSQLdb() {
-		try {
-			Class.forName(this.driver).newInstance();
-			this.conn = DriverManager.getConnection(this.url,this.user,this.passwd);
-			System.out.println("Connected to the DB");
-		}
-		catch(Exception e){
-			System.out.println("Exception "+ e.getMessage());
-		}
-	}
-	
-	public void setUserInfo(String email,String password,String username) {
-		String query = "insert into mezutaula.users values ('" + email + "', '" + password + "', '" + username + "');";
-		System.out.println("\tDB query: "+ query);
-		try {
-			Statement st = this.conn.createStatement();
-			st.executeUpdate("Query succesfull !!!!");
-		}
-		catch(Exception e) {
-			System.out.println("Exception: "+e.getMessage());
-		}
-	}
-	
-	public String getUsername(String email, String password) {
-		String query = "select username from mezutaula.users where email = '" + email + "' and password = '" + password;
-		System.out.println("\tDB query: "+ query);
-		String username = null;
-		
-		try {
-			Statement st = this.conn.createStatement();
-			ResultSet rs = st.executeQuery(query);
-			while(rs.next()) {
-				username = rs.getString("username");
-			}
-			System.out.println("\tQuery succesfull !!!");
-		}
-		catch(Exception e) {
-			System.out.println("Exception: "+e.getMessage());
-		}
-		return username;
-		
-	}
-	
-	public void setMessageInfo(String message, String username) {
-		String query = "insert into mezutaula.messages values ('0', '" + message + "', '" + username + "')";
-		System.out.println("\tDB query: "+ query);
-		
-		try {
-			Statement st = this.conn.createStatement();
-			st.executeUpdate(query);
-			System.out.println("\tQuery succesfull !!!");
-		}
-		catch(Exception e) {
-			System.out.println("Exception: "+e.getMessage());
-		}
-	}
-	
-	public ArrayList<MessageInfo> getAllMessages(){
-		String query = "select * from mezutaula.messages";
-		System.out.println("\tDB query: "+ query);
-		ArrayList<MessageInfo> messageInfoList = new ArrayList<MessageInfo>();
-		
-		try {
-			Statement st = this.conn.createStatement();
-			ResultSet rs = st.executeQuery(query);
-			while(rs.next()) {
-				messageInfoList.add(new MessageInfo(rs.getString("message"),rs.getString("message")));
-			}
-			System.out.println("\tQuery succesfull !!!");
-		}
-		catch(Exception e) {
-			System.out.println("Exception: "+e.getMessage());
-		}
-		return messageInfoList;
-	}
+	   public MYSQLdb() {
+		   try {
+		       Class.forName(this.driver).newInstance();
+		       this.conn = DriverManager.getConnection(this.url,this.user,this.passwd);
+		       System.out.println("Connected to DB!!!");
+		   } catch(Exception e) {
+		        System.out.println("Exception: " + e.getMessage());
+		   }
+	   }
+	   
+	   public void setUserInfo(String email, String password, String username) {
+		   String query = "INSERT INTO mezutaula.users VALUES ('" + email + "', '" + password + "', '" + username + "');";
+		   System.out.println("\tDB query: " + query);
+		   try {
+		         Statement st = this.conn.createStatement();
+		         st.executeUpdate(query); 
+		         System.out.println("Query successful!!!");
+		       } catch(Exception e) {
+		          System.out.println("Exception: " + e.getMessage());
+		      }
+
+	   }
+	   public String getUsername(String email, String password) {
+		   String query = "SELECT username FROM mezutaula.users WHERE email='" + email + "' AND password='" + password + "';";
+		   System.out.println("\tDB query: " + query);
+		   String username = null;
+		   try {
+		         Statement st = this.conn.createStatement();
+		         ResultSet res = st.executeQuery(query); 
+		         while(res.next()) {
+		            username = res.getString("username");
+		          }
+		         System.out.println("\tQuery successful!!!");
+		       } catch(Exception e) {
+		          System.out.println("Exception: " + e.getMessage());
+		      }
+		   return username;
+	   }
+	   
+	   public void setMessageInfo(String message, String username) {
+		   String query = "INSERT INTO mezutaula.messages VALUES ('0', '" + message + "', '" + username + "');";
+		   System.out.println("\tDB query: " + query);
+		   try {
+		         Statement st = this.conn.createStatement();
+		         st.executeUpdate(query); 
+		         System.out.println("\tQuery successful!!!");
+		       } catch(Exception e) {
+		          System.out.println("Exception: " + e.getMessage());
+		      }
+	   }
+	   
+	   public ArrayList<MessageInfo> getAllMessages() {
+		      String query = "SELECT * FROM mezutaula.messages;";
+		      System.out.println("\tDB query: " + query);
+		      ArrayList<MessageInfo> messageInfoList = new ArrayList<MessageInfo>();
+		      
+		      try {
+		         Statement st = this.conn.createStatement();
+		          ResultSet res = st.executeQuery(query); 
+		          while(res.next()) {
+		             messageInfoList.add(new MessageInfo(res.getString("message"), res.getString("username")));
+		          }   
+		          System.out.println("\tQuery successful!!!");
+		       } catch(Exception e) {
+		          System.out.println("Exception: " + e.getMessage());
+		      }
+		      return messageInfoList;
+		   }
 
 }
